60bf38018e8d11365b8fda6ec3f24a15b0ddadc6
D1eg0Verdin <diego.verdinramirez23@ycdsbk12.ca>
2022-05-12 23:08:51 -0400

    final submission

--- Sketch.java
+++ Sketch.java
@@ -1,36 +1,163 @@
 import processing.core.PApplet;
 
 public class Sketch extends PApplet {
-	
-	
-  /**
-   * Called once at the beginning of execution, put your size all in this method
-   */
+ 
+  float[] circleX = new float[35];
+  float[] circleY = new float[35];
+  boolean[] ballHideStatus = new boolean[35];
+  int intSnowballSpeed = 2;
+
+  float fltPlayerX = 400;
+  float fltPlayerY = 800;
+
+  boolean boolUpPressed = false;
+  boolean boolDownPressed = false;
+  boolean boolLeftPressed = false;
+  boolean boolRightPressed = false;
+
+  boolean boolPlayerStatus = true;
+  int intLives = 3;
+
+  boolean boolMouseClicked = false;
+ 
+
   public void settings() {
-	// put your size call here
-    size(400, 400);
+    size(800,800);
   }
 
-  /** 
-   * Called once at the beginning of execution.  Add initial set up
-   * values here i.e background, stroke, fill etc.
-   */
+ 
   public void setup() {
-    background(210, 255, 173);
+    for (int i = 0; i < circleY.length; i++) {
+      circleY[i] = random(height);
+      circleX[i] = random(width);
+      ballHideStatus[i] = false;
+    }
+  
+  }
+  public void draw() {
+  // check to see if player has not died
+  if (boolPlayerStatus == true) {
+    background(50);
+  
+    fill(255);
+
+    // draw snowballs to the screen if ballHideStatus is false
+    for (int i = 0; i < circleY.length; i++) {
+      if (ballHideStatus[i] == false) {
+        ellipse(circleX[i], circleY[i], 50, 50);
+
+        circleY[i] += intSnowballSpeed;
+      }
+  
+     
+      if (circleY[i] > height - 25) {
+        circleY[i] = 0;
+      }
+
+      // if player circle collides with snowball, stop drawing snowball to screen and remove 1 life
+      if (dist(fltPlayerX, fltPlayerY, circleX[i], circleY[i]) <= 37.5 && ballHideStatus[i] == false) {
+        ballHideStatus[i] = true;
+        intLives--;
+      }
+      
+      // if snowball is clicked, delete it
+      if (dist(mouseX, mouseY, circleX[i], circleY[i]) <= 25 && boolMouseClicked) {
+          ballHideStatus[i] = true;
+      }
+      
+    }
+
+    fill(0, 0, 255);
+
+    // draw player circle
+    ellipse(fltPlayerX, fltPlayerY, 25, 25);
+    
+    // keyboard controls for player using w, a, s, d
+    if (boolLeftPressed) {
+      fltPlayerX += -5;
+    }
+    if (boolRightPressed) {
+      fltPlayerX += 5;
+    }
+    if (boolUpPressed) {
+      fltPlayerY += -5;
+    }
+    if (boolDownPressed) {
+      fltPlayerY += 5;
+    }
+
+    fill(246, 7, 17);
+
+    
+    for (int i = 1; i <= intLives; i++) {
+      rect(50 * i, 50, 50, 50);
+    }
+
+    if (intLives == 0) {
+      boolPlayerStatus = false;
+    }
   }
 
-  /**
-   * Called repeatedly, anything drawn to the screen goes here
-   */
-  public void draw() {
-	  
-	// sample code, delete this stuff
-    stroke(128);
-    line(150, 25, 270, 350);  
-
-    stroke(255);
-    line(50, 125, 70, 50);  
+  // screen turns white when player dies
+  else {
+    background(255);
   }
   
-  // define other methods down here.
+  }
+  public void keyPressed() {
+    if (key == 'a')  {
+      boolLeftPressed = true;
+    }
+    else if (key == 'd') {
+      boolRightPressed = true;
+    }
+    else if (key == 'w') {
+      boolUpPressed = true;
+    }
+    else if (key == 's') {
+      boolDownPressed = true;
+    }
+
+    if (keyCode == UP) {
+      intSnowballSpeed = 1;
+    }
+    if (keyCode == DOWN) {
+      intSnowballSpeed = 5;
+    }
+  }
+
+  
+  public void keyReleased() {
+    if (key == 'a')  {
+      boolLeftPressed = false;
+    }
+    else if (key == 'd') {
+      boolRightPressed = false;
+    }
+    else if (key == 'w') {
+      boolUpPressed = false;
+    }
+    else if (key == 's') {
+      boolDownPressed = false;
+    }
+
+    if (keyCode == UP) {
+      intSnowballSpeed = 2;
+    }
+    
+    if (keyCode == DOWN) {
+      intSnowballSpeed = 2;
+    }
+  }
+
+
+  public void mousePressed() {
+    boolMouseClicked = true;
+  }
+
+  public void mouseReleased() {
+    boolMouseClicked = false;
+  }
+  
+  
 }
